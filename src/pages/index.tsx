dc0c4dd70ddefab0e445e3c5b8d45d0c976f736e
edualt <eduardosebastianhdz503@gmail.com>
2023-02-25 17:26:10 -0600

    feat(pages): set home page template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,38 @@ import { socket, WebSocketProvider } from "@/contexts/WebSocketContext";
 import { Inter } from "@next/font/google";
 import { FC } from "react";
 import { useEffect } from "react";
-import { WebSocket } from "@/components/websocket/WebSocket";
+import {
+  Container,
+  SectionLeft,
+  SectionRight,
+  Header,
+} from "../styles/GlobalComponents";
+import { RoomBox, RoomContainer } from "@/components/Rooms/RoomStyles";
+import Rooms from "@/components/Rooms/Rooms";
+import Chat from "@/components/Chat/Chat";
+import Input from "@/components/Input/Input";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Home: FC = () => {
   return (
     <WebSocketProvider value={socket}>
-      <WebSocket/>
-  </WebSocketProvider>
+      <Container>
+        <SectionLeft>
+          <Header>
+            <h1>Contacts</h1>
+          </Header>
+          <Rooms/>
+        </SectionLeft>
+        <SectionRight>
+          <Header>
+            <h1>Chat</h1>
+          </Header>
+          <Chat />
+          <Input />
+        </SectionRight>
+      </Container>
+    </WebSocketProvider>
   );
 };
 
