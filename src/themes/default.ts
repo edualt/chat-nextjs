dc0fae13b313a742cecfe4a5fc274be1cab9ab8e
edualt <eduardosebastianhdz503@gmail.com>
2023-02-25 17:28:20 -0600

    feat(styles): add default config for styling

---
+++ src/themes/default.ts
@@ -0,0 +1,13 @@
+export default {
+    fonts: {
+        title: "Space Grotesk, sans-serif",
+        main: "Space Grotesk, sans-serif"
+    },
+    colors: {
+        primary: "hsl(204,23.8%,95.9%)",
+        background: "#191819",
+        accent: "hsl(34.9,98.6%,72.9%)",
+        button: "hsl(205.1,100%,36.1%)",
+        background2: "#161516",
+    }
+}
