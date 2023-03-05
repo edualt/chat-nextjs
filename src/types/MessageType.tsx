063fb48b07ba7b52cf4b11fc2afba31644d397a6
edualt <eduardosebastianhdz503@gmail.com>
2023-03-04 19:54:22 -0600

    feat(types): add custom types

---
+++ src/types/MessageType.tsx
@@ -0,0 +1,8 @@
+type MessageType = {
+  username: string;
+  message: string;
+  file: ArrayBuffer;
+  type: string;
+};
+
+export default MessageType;
---
+++ src/types/UserContextType.ts
@@ -0,0 +1,3 @@
+export type UserContextType = {
+  username: string;
+};
