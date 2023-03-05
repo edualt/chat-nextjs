86b05f75703057b23cb72a7a530527f7779ba74f
edualt <eduardosebastianhdz503@gmail.com>
2023-03-04 19:53:44 -0600

    feat(app): add index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,50 @@
-import { socket, WebSocketProvider } from "@/contexts/WebSocketContext";
-import { Inter } from "@next/font/google";
 import { FC } from "react";
-import { useEffect } from "react";
+import { useState } from "react";
 import {
   Container,
   SectionLeft,
   SectionRight,
   Header,
 } from "../styles/GlobalComponents";
-import { RoomBox, RoomContainer } from "@/components/Rooms/RoomStyles";
 import Rooms from "@/components/Rooms/Rooms";
 import Chat from "@/components/Chat/Chat";
 import Input from "@/components/Input/Input";
-
-const inter = Inter({ subsets: ["latin"] });
+import Modal from "@/components/Modal/Modal";
+import { UserContext } from "@/contexts/userContext";
+import MessageType from "@/types/MessageType";
 
 const Home: FC = () => {
+  const [showModal, setShowModal] = useState(true);
+  const [username, setUsername] = useState("");
+  const [messages, setMessages] = useState<MessageType[]>([]);
+
+
+
   return (
-    <WebSocketProvider value={socket}>
-      <Container>
+    <>
+    <UserContext.Provider value={{username}}>
+      {showModal ? (
+        <Modal showModal={showModal} setShowModal={setShowModal} setUsername={setUsername}></Modal>
+      ) : (
+        <Container>
         <SectionLeft>
           <Header>
             <h1>Contacts</h1>
           </Header>
-          <Rooms/>
+          <Rooms setMessages={setMessages}/>
         </SectionLeft>
         <SectionRight>
           <Header>
             <h1>Chat</h1>
           </Header>
-          <Chat />
-          <Input />
+          <Chat username={username} messages={messages} setMessages={setMessages}/>
+          <Input username={username}/>
         </SectionRight>
       </Container>
-    </WebSocketProvider>
+      )}
+      </UserContext.Provider>
+      </>
+      
   );
 };
 
